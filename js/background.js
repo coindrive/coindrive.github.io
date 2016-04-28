ba67ccef7f50fc4f2e6cc9765b23a038927ea491
coindrive <1demedik1988@gmail.com>
2016-04-29 01:15:17 +0300

    Update background.js

--- js/background.js
+++ js/background.js
@@ -1,10 +1,3 @@
-/*!
- * Webogram v0.5.2 - messaging web application for MTProto
- * https://github.com/zhukov/webogram
- * Copyright (C) 2014 Igor Zhukov <igor.beatle@gmail.com>
- * https://github.com/zhukov/webogram/blob/master/LICENSE
- */
-
 chrome.app.runtime.onLaunched.addListener(function(launchData) {
   chrome.app.window.create('../index.html', {
   	id: 'webogram-chat',
